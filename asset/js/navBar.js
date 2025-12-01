782594d030312acd937c9b79ab0dfcd182a2a13f
carlcain <carljustinecain@gmail.com>
2025-12-01 12:50:05 +0800

    added function on navbar

---
+++ asset/js/navBar.js
@@ -0,0 +1,36 @@
+document.getElementById("home").addEventListener("click", function () {
+    window.location.href = "home.html"; 
+});
+
+document.getElementById("daily-care").addEventListener("click", function () {
+    window.location.href = "dailyCare.html"; 
+});
+
+document.getElementById("fresh-breath").addEventListener("click", function () {
+    window.location.href = "freshbreath.html"; 
+});
+
+document.getElementById("dental-tools").addEventListener("click", function () {
+    window.location.href = "dentaltools.html"; 
+});
+
+document.getElementById("about").addEventListener("click", function () {
+    window.location.href = "about.html"; 
+});
+
+document.getElementById("reviews").addEventListener("click", function () {
+    window.location.href = "reviews.html"; 
+});
+
+document.getElementById("search").addEventListener("click", function () {
+    window.location.href = "reviews.html"; 
+});
+
+document.getElementById("cart").addEventListener("click", function () {
+    window.location.href = "Cart.html"; 
+});
+
+document.getElementById("user").addEventListener("click", function () {
+    window.location.href = "profile.html"; 
+});
+

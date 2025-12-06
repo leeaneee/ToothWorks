6d451ae367bf319a2588b69480b57037f17116bd
carlcain <carljustinecain@gmail.com>
2025-12-06 11:57:26 +0800

    Cain json (#24)
    * added json links for the footer (footer getter realy thats the best thing i can come up with)
    
    * sign up update

--- asset/js/ItemGetter.js
+++ asset/js/ItemGetter.js
@@ -1,17 +1,3 @@
-// async function getItem(name) {
-//   const res = await fetch("asset/json/items.json");
-//   const data = await res.json();
-//   return data[name]; 
-// }
-
-// window.addEventListener("DOMContentLoaded", async () => {
-//   const item = await getItem("toothbrush");
-
-//   document.getElementById("toothbrush").src = item.img;
-//   document.getElementById("toothbrush-info").textContent = item.info;
-//   document.getElementById("toothbrush-price").textContent = item.price;
-//   document.getElementById("toothbrush-rating").textContent = item.rating;
-// });
 
 window.addEventListener("DOMContentLoaded", async () => {
   const res = await fetch("asset/json/items.json");
